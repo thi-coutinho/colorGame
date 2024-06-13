21668615bc5a7e6e48c90fa3cf26e38d132232d2
thiagocsantos <thi.coutinho.santos@gmail.com>
2024-06-13 20:31:48 -0300

    gam colors

---
+++ main.js
@@ -0,0 +1,35 @@
+// import './style.css';
+const buttons = document.querySelectorAll('.button');
+console.log(buttons);
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log('cliquei');
+    const id = parseInt(button.id.replace('btn', ''));
+    toggleColors(id);
+  });
+});
+
+function toggleColors(id) {
+  const neighbors = getNeighbors(id);
+
+  neighbors.forEach(neighbor => {
+    const btn = document.getElementById(`btn${neighbor}`);
+    btn.classList.toggle('rosa');
+  });
+}
+
+function getNeighbors(id) {
+  const neighbors = {
+    1: [1, 2, 4, 5],
+    2: [1, 2, 3, 4, 5, 6],
+    3: [2, 3, 5, 6],
+    4: [1, 2, 4, 5, 7, 8],
+    5: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    6: [2, 3, 5, 6, 8, 9],
+    7: [4, 5, 7, 8],
+    8: [4, 5, 6, 7, 8, 9],
+    9: [5, 6, 8, 9]
+  };
+  return neighbors[id];
+}
+
